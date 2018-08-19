0456b43f1c8cd5ae5e83748e93f615d545061a66
Omar Baqueiro <obaqueiro@paystand.com>
2018-08-18 22:39:38 -0500

    Add parseInt reloaded code

---
+++ parseInt_reloaded_525c7c5ab6aecef16e0001a5.js
@@ -0,0 +1,40 @@
+function parseInt(string) {
+  var values = string.split(' ');
+  var total = 0;
+  var current = 0;
+  const digits = { 
+    'one' : 1,  'two' : 2, 'three' : 3, 'four' : 4, 'five' : 5,
+    'six' : 6, 'seven' : 7, 'eight' : 8, 'nine' : 9, 'ten' : 10  ,
+    'eleven': 11, 'twelve':12, 'thirteen': 13, 'fourteen': 14, 'fifteen': 15,
+    'sixteen': 16, 'seventeen': 17, 'eighteen': 18, 'nineteen': 19, 'twenty': 20,
+    'thirty': 30, 'forty': 40, 'fifty': 50, 'sixty': 60, 'seventy': 70, 
+    'eighty': 80, 'ninety': 90
+    }
+    var multipliers = { 
+      'hundred': 100, 'thousand':1000, 'million': 1000000
+    }
+    
+  while(values.length > 0) {
+    var token = values.shift();
+    var multiplier = 1;
+    if (token == 'zero') { return 0; }    
+    if (token.includes('-')) {
+      token = token.split('-');
+       current += digits[token[0]];
+       current += digits[token[1]];
+    }
+    else {
+      current += digits[token];
+    }
+    
+    if (Object.keys(multipliers).includes(values[0])){
+      multiplier = multipliers[values.shift()];
+    }
+    
+    total += (current * multiplier);
+    current = 0;
+  }
+  return total;
+}:
+
+
