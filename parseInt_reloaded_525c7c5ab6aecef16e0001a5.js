5a36304debae27c6f332897bb34c2b975fcd38bb
Omar Baqueiro <obaqueiro@paystand.com>
2018-08-18 22:58:23 -0500

    fix parseInt to account for all cases

--- parseInt_reloaded_525c7c5ab6aecef16e0001a5.js
+++ parseInt_reloaded_525c7c5ab6aecef16e0001a5.js
@@ -12,12 +12,13 @@ function parseInt(string) {
     }
     var multipliers = { 
       'hundred': 100, 'thousand':1000, 'million': 1000000
-    }
+      }
     
   while(values.length > 0) {
     var token = values.shift();
     var multiplier = 1;
     if (token == 'zero') { return 0; }    
+    if (token == 'and') { continue;}
     if (token.includes('-')) {
       token = token.split('-');
        current += digits[token[0]];
@@ -27,14 +28,28 @@ function parseInt(string) {
       current += digits[token];
     }
     
-    if (Object.keys(multipliers).includes(values[0])){
-      multiplier = multipliers[values.shift()];
+    while( Object.keys(multipliers).includes(values[0]) ) {
+     multiplier *= multipliers[values.shift()];
     }
-    
-    total += (current * multiplier);
+    if (total >= 1000) {  total /=multiplier; }
+    total += current;
+    total *= multiplier;
     current = 0;
+    multiplier = 1;
   }
   return total;
-}:
+}
 
 
+// Tests
+Test.assertEquals(parseInt('zero'), 0);
+Test.assertEquals(parseInt('one million and two'), 1000002);
+Test.assertEquals(parseInt('one thousand three hundred and thirty-seven'), 1337);
+Test.assertEquals(parseInt('nine hundred twenty-five thousand'), 925000);
+Test.assertEquals(parseInt('nine hundred twenty-five thousand seven hundred twenty two'), 925722);
+
+Test.assertEquals(parseInt('one'), 1);
+Test.assertEquals(parseInt('forty-six'), 46);
+Test.assertEquals(parseInt('seventy-nine'), 79);
+Test.assertEquals(parseInt('twenty'), 20);
+Test.assertEquals(parseInt('two hundred forty-six'), 246);
